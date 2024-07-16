15ff129063ad9aba310d303cce159c432d313ea8
Lucy <rafploooz@gmail.com>
2024-07-16 02:36:40 -0400

    Make Work

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders submit button', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = document.getElementById("submit");
   expect(linkElement).toBeInTheDocument();
 });
