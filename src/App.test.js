179d6af605dc9b7f7f128833e8c7f6fbc8d3b901
avg <heyoitsflowey@gmail.com>
2024-07-31 23:46:26 -0400

    next.js

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders submit button', () => {
-  render(<App />);
-  const linkElement = document.getElementById("submit");
-  expect(linkElement).toBeInTheDocument();
-});
