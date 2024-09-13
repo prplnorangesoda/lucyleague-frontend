da3af0c8793f1fed6dae0fe21f358391fad60ffd
Lucy <rafploooz@gmail.com>
2024-09-13 17:49:29 -0400

    Should now work on ipv6

--- app/globals.js
+++ app/globals.js
@@ -2,5 +2,17 @@
 
 export default {
 	API_MIDDLE: `:8080/api/v1/`,
-	trim_port: (str) => str.substring(0, str.indexOf(':', 7)),
+	/**
+	 * Should not be used if there is no port.
+	 *
+	 * @param {string} str
+	 */
+	trim_port: (str) => {
+		// FEAR
+		if (!str.match(/https?:\/\/([0-9a-zA-Z\.]*|\[[0-9a-f:]*\]):[0-9]+/))
+			return str;
+		let reverse = str.split('').reverse().join('');
+		let index = str.length - reverse.indexOf(':') - 1; // to include the : itself
+		return str.substring(0, index);
+	},
 };
