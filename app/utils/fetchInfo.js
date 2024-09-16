5d393a2cefe3159122dd94ddb429b86483a7d106
avg <heyoitsflowey@gmail.com>
2024-09-16 18:37:58 -0400

    update helpers

--- app/utils/fetchInfo.js
+++ app/utils/fetchInfo.js
@@ -1,25 +1,68 @@
 'use client'
 
-import React, { useState, useEffect } from 'react';
-import { useCookies } from 'react-cookie';
 import globals from '../globals';
 
-export function FetchInfoFromAuth() {
-    const [cookies] = useCookies(['auth-token']);
-	const authToken = cookies['auth-token'];
-	const [AuthInfo, setAuthInfo] = useState(null);
+export function FetchInfoFromAuth(token, callback) {
+	var AuthInfo
 
-	useEffect(() => {
-		// we are running on the client side - hostname unnecessary
-		const url = globals.trim_port(window.location.origin) + globals.API_MIDDLE + 'user/authtoken/';
-		if (authToken) {
-			fetch(url + authToken)
-				.then((res) => res.json())
-				.then((data) => {
-					setAuthInfo(data);
-				})
-		}
-	}, []);
+	// we are running on the client side - hostname unnecessary
+	const url = globals.trim_port(window.location.origin) + globals.API_MIDDLE + 'user/authtoken/';
+	if (token) {
+		fetch(url + token)
+			.then((res) => res.json())
+			.then((data) => {
+				AuthInfo = data;
+
+				// run the callback
+				if (callback) { 
+					if ( typeof callback !== "function" ) {
+						console.warn("Callback is not a function!")
+					} else {
+						callback(data)
+					}
+				}
+			})
+	}
 	
     return AuthInfo
+}
+
+export function FetchUserInfoFromS64( s64, callback, error_callback ) {
+	var UserInfo
+
+	if (!s64) {
+		return
+	}
+
+	const url =
+		globals.trim_port(window.location.origin) +
+		globals.API_MIDDLE +
+		'user/steamid/';
+	const query = url + s64;
+	
+	fetch(query)
+		.then((res) => res.json())
+		.then((data) => {
+			UserInfo = data;
+			
+			if (callback) { 
+				if ( typeof callback !== "function" ) {
+					console.warn("Callback is not a function!")
+				} else {
+					callback(data)
+				}
+			}
+		})
+		.catch((err) => {
+			console.log(err.message);
+			if (error_callback) { 
+				if ( typeof error_callback !== "function" ) {
+					console.warn("Callback is not a function!")
+				} else {
+					error_callback(err)
+				}
+			}
+		});	
+
+	return UserInfo
 }
